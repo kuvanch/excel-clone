42dc3ee3ebc4cbd88a65b59b14349c72461b2ab9
kuvanch <kolyabek13@gmail.com>
2021-04-04 21:09:35 +0500

    added plugins html clean copy webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname,'src'),
@@ -6,6 +9,18 @@ module.exports = {
     mode: 'development',
     output:{
         path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js'
-    }
+        filename: 'bundle.[hash].js'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [{ 
+                from:  path.resolve(__dirname,'src/favicon.ico'), 
+                to: path.resolve(__dirname,'dist') 
+                }],
+        }),
+    ]
 }
