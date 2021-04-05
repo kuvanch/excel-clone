a9be8ef265b5f2f209cb2e79728498172164b990
kuvanch <kolyabek13@gmail.com>
2021-04-05 21:04:12 +0500

    add sass-loader babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// const isProd = process.env.NODE_ENV === 'production'
+// const isDev = !isProd
+// console.log(isProd);
+// const filename = name => isProd ? `bundle.[hash].${name}` : `bundle.${name}`
 
 module.exports = {
     context: path.resolve(__dirname,'src'),
@@ -9,11 +15,21 @@ module.exports = {
     mode: 'development',
     output:{
         path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.[hash].js'
+        filename: 'bundle.[chunkhash].js'
+    },
+    resolve:{
+        alias: {
+            '@': path.resolve(__dirname,'src'),
+            '@/core':path.resolve(__dirname,'src/core')
+        },
+        extensions: ['.js']     
     },
     plugins:[
         new HtmlWebpackPlugin({
             template: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.[chunkhash].css'
         }),
         new CleanWebpackPlugin(),
         new CopyPlugin({
@@ -22,5 +38,24 @@ module.exports = {
                 to: path.resolve(__dirname,'dist') 
                 }],
         }),
-    ]
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.s[ac]ss$/i,
+            use: [MiniCssExtractPlugin.loader,"css-loader","sass-loader",
+            ],
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ],
+      },
 }
